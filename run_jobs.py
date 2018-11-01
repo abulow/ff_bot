779b3a6c9b7bf0c6f26b608ac41df0815bddd562
Adam Bulow <abulow@uchicago.edu>
2018-11-01 19:37:10 -0400

    Changed automatic run_jobs printing to command line to be only every 15th run.

--- run_jobs.py
+++ run_jobs.py
@@ -7,14 +7,16 @@ def run():
     scoreboard_start_times = [first_scoreboard_start_time + timedelta(weeks=i) for i in range(16)]
     week_nums = {x: str(idx+1) for idx, x in enumerate(scoreboard_start_times)}
     remaining_scoreboard_start_times = [x for x in scoreboard_start_times if x > datetime.now()]
+    i = 0
     while datetime.now() < end_of_season:
         run_bot('transactions')
         if remaining_scoreboard_start_times and (datetime.now() > remaining_scoreboard_start_times[0]):
             week_num = week_nums[remaining_scoreboard_start_times[0]]
             run_bot('scores', week_num=week_num)
             remaining_scoreboard_start_times = remaining_scoreboard_start_times[1:]
-        print()
-        print('Updated, Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.now()))
+        if (i % 15) == 0:
+            print()
+            print('Updated, Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.now()))
         time.sleep(60)
 
 if __name__ == '__main__':
